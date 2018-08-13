642301e8ff5384d457b84fa6aa4ebc11299df915
Shreyansh Dubey <shreyanshece1041@gmail.com>
2018-08-13 22:24:11 +0530

    Update Experiment4.m

--- Experiment4.m
+++ Experiment4.m
@@ -1,24 +1,25 @@
+%%note knee measurement has been commented in most of the functions of these people as it was giving errors so the data has been taken excluding knee for some
 clc
 clear
-i=37; %always +2 of prev value
-thresh=7;
-% Santosh(i,thresh); 
-% Dileep(i,thresh);
-% Ravi(i,thresh); 
-% Hemanth(i,thresh);
-% Nagraj(i,thresh);
-% Madesh(i,thresh);
-% Mallickarjun(i,thresh);
-% Shyamsundar(i,thresh);
-% Rajshekhar(i,thresh);
-% Rajeshkumar(i,thresh);
-% Raju(i,thresh);
-% Velu(i,thresh);
-% Raghvendra(i,thresh);
-% Vijaymishra(i,thresh);
-% Rashid(i,thresh);
-% Ramanjunipava(i,thresh);
-% Arshad(i,thresh);
+i=37; %always +2 of prev value %%this is the count value that defines the column number where to write start writing the data
+thresh=7; 
+ Santosh(i,thresh); 
+ Dileep(i,thresh);
+ Ravi(i,thresh); 
+ Hemanth(i,thresh);
+ Nagraj(i,thresh);
+ Madesh(i,thresh);
+ Mallickarjun(i,thresh);
+ Shyamsundar(i,thresh);
+ Rajshekhar(i,thresh);
+ Rajeshkumar(i,thresh);
+ Raju(i,thresh);
+ Velu(i,thresh);
+ Raghvendra(i,thresh);
+ Vijaymishra(i,thresh);
+ Rashid(i,thresh);
+ Ramanjunipava(i,thresh);
+ Arshad(i,thresh);
 Manojkumar(i,thresh);
 Sailesh(i,thresh);
 Lingappa(i,thresh);
