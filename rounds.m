32b30a0e3328bcb26007c028fc8b728911be3359
Shreyansh Dubey <shreyanshece1041@gmail.com>
2018-08-13 22:44:58 +0530

    Add files via upload

---
+++ rounds.m
@@ -0,0 +1,9 @@
+function [newVal] = rounds(x) 
+    dist = mod(x, 0.5); 
+    floorVal = x - dist; 
+    if  dist  >=0.25
+        newVal = floorVal + 0.5;      
+    else
+        newVal = floorVal;
+end
+
