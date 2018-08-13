513456f133c1f008da3850f3c876f2e8eeb18e6d
Shreyansh Dubey <shreyanshece1041@gmail.com>
2018-08-13 22:10:58 +0530

    Add files via upload

---
+++ Experiment4.m
@@ -0,0 +1,28 @@
+clc
+clear
+i=37; %always +2 of prev value
+thresh=7;
+% Santosh(i,thresh); 
+% Dileep(i,thresh);
+% Ravi(i,thresh); 
+% Hemanth(i,thresh);
+% Nagraj(i,thresh);
+% Madesh(i,thresh);
+% Mallickarjun(i,thresh);
+% Shyamsundar(i,thresh);
+% Rajshekhar(i,thresh);
+% Rajeshkumar(i,thresh);
+% Raju(i,thresh);
+% Velu(i,thresh);
+% Raghvendra(i,thresh);
+% Vijaymishra(i,thresh);
+% Rashid(i,thresh);
+% Ramanjunipava(i,thresh);
+% Arshad(i,thresh);
+Manojkumar(i,thresh);
+Sailesh(i,thresh);
+Lingappa(i,thresh);
+RamKrishna(i,thresh);
+Yogesh(i,thresh);
+Prabhu(i,thresh);
+
