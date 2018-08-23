f77d90f1831fd66f4f0f5f7902cbbb9718d7d648
Shreyansh Dubey <shreyanshece1041@gmail.com>
2018-08-23 15:01:34 +0530

    Update process.m

--- process.m
+++ process.m
@@ -1,8 +1,27 @@
-function []= process(filename1,filename2,filename3,filename4,height,th,clm)
+function []= process(filename1,filename2,filename3,filename4,height,th,clm)   %%This is demonstration function now we use every person as a different function refer to Arshad.m
+if count<26
+    Stringvect = ('B':'Z');
+    posColumn = [Stringvect(count) '4'];
+elseif count>26 && count<52
+    count2=count-26;
+    Stringvect = ('A':'Z');
+    posColumn = ['A' Stringvect(count2) '4'];
+elseif count>52 && count<78
+    count3=count-52;
+    Stringvect = ('A':'Z');
+    posColumn = ['B' Stringvect(count3) '4'];
+else
+    fprintf('Error: Count limit exceeds than what is defined, please define higher limits in code');
+end
 
-kk=7; 
-%height=65.5;
-
+% kk=7; 
+%height=67;
+%filename1='hemanthf1.csv';  %%give here any filename
+%filename2='hemanths1.csv';
+%filename3='hemanthf2.csv';
+%filename4='hemanths2.csv'; 
+S = strsplit(filename1,'f');
+disp(S);
 
 A1=initiate2(filename1,kk);
 %
@@ -32,8 +51,8 @@ cro=crotchfn(a1,C,X1,Y1);
 hip =hipfn(C,X2,Y2);
 hiplevel=y2(hip);
 %
-[location2,p1]=new_right_points(a1,cro,hiplevel);
-[location,p2]=new_left_points(a1,cro,hiplevel);
+[location2,p1]=new_right_points3(a1,cro,hiplevel);
+[location,p2]=new_left_points3(a1,cro,hiplevel);
 leftloc=location;
 rightloc=location2;
 X=X1;
@@ -58,21 +77,65 @@ ps=loc2(X2,Y2,l);
 %%If the below 5 commented lines run without any error for a particular img
 %%then the neck point is correctly mapped....for some images it is not
 %%going fine it runs with errors for those images we have commented it
-
-% [~,~,pneck]=newsideneck(a2,l);
-% ps(1,1,1)=pneck(1,1);
-% ps(1,1,2)=pneck(1,2);
-% ps(1,2,1)=pneck(2,1);
-% ps(1,2,2)=pneck(2,2);
+try
+    [~,~,pneck]=newsideneck0(a2,l);
+    ps0(1,1,1)=pneck(1,1);
+    ps0(1,1,2)=pneck(1,2);
+    ps0(1,2,1)=pneck(2,1);
+    ps0(1,2,2)=pneck(2,2);
+catch
+    fprintf('error with newsideneck0.....risk element\n');
+end
+n1 = 0;
+n2 = 0;
+try
+    [~,~,pneck]=newsideneck(a2,l);
+    ps(1,1,1)=pneck(1,1);
+    ps(1,1,2)=pneck(1,2);
+    ps(1,2,1)=pneck(2,1);
+    ps(1,2,2)=pneck(2,2);n1 = realneck2(filename1,filename2,height,kk);
+    fprintf('Came out of realneck, n1 is %f\n',n1);
+    n2 = t*measureneck(P,ps);
+    fprintf('Calculated n2 as %f, no error in sideneck()\n',n2);
+    %n3 = NeckCalc2(filename1,filename2,height);
+    %fprintf('Calculated n3 as %f, no error in NeckCalc()\n',n3);
+    args = [n1, n2];  %%add here n3 for Con.A
+    arr = valbw(args);
+    neck = mean(arr);
+    fprintf('Calculated neck as mean(arr) successfully');
+    
+catch
+    fprintf('error in try section\n');  
+    try
+        nk =  neckhandle3(filename1,filename2,height,P,ps,t,ps0);
+        nck = NeckCalc2(filename1,filename2,height);
+        neck = (nk + nck)/2;
+        
+    catch
+        try
+            fprintf('Another try for rescue\n');
+            nck1 =  neckhandle4(filename1,filename2,height);
+            nck2 = NeckCalc2(filename1,filename2,height);
+            neck = (nck1+nck2)/2;
+            
+        catch
+            fprintf('Final try for rescue\n');
+            neck = NeckCalc2(filename1,filename2,height);
+        end
+        
+    end
+end   
+ValuesInInches(8)=neck;    
 
 % measuring
 t1=measurethigh(P,ps);
 ValuesInInches(12)=t1*t;
-ValuesInInches(8)=realneck(filename1,filename2,height,th);
+%ValuesInInches(8)=NeckCalc(filename1,filename2,height);
+%ValuesInInches(8)=neckhandle2(filename1,filename2,height,P,ps,t);
 %ValuesInInches(8)=t*measureneck(P,ps);
 t3=measureinseam(X1,Y1,x1,y1,cro);
 ValuesInInches(14)=t3*t;
-t4=measurehip(P,ps);
+t4=measurehip2(P,ps);
 ValuesInInches(7)=t4*t;
 ValuesInInches(11)=ValuesInInches(7);
 t5=measurechest(P,ps);
@@ -80,9 +143,9 @@ ValuesInInches(5)=t5*t;
 t6= measurenbicep(leftloc,rightloc,X1,Y1);
 ValuesInInches(4)=t6*t;
 t7=measuretrouserwaist(P,ps);
-ValuesInInches(6)=t7*t;
 t8=measureshirtwaist(P,ps);
-ValuesInInches(10)=t8*t;
+ValuesInInches(6)=t7*t; %initially was t7*t  %unswapped
+ValuesInInches(10)=t8*t; %initially was t8*t
 t9=measureoutseam(X1,Y1,cro,P);
 ValuesInInches(9)=t9*t;
 %
@@ -94,25 +157,10 @@ t11=measureshoulder(a3);
 ValuesInInches(2)=t11*t;
 ValuesInInches(13)=measureknee(a5,t);
 ValuesInInches=ValuesInInches';
-
-
-%Comment the line which plot later 
-rr=1:7;
-figure;
-imshow(a1);
-hold on
-plot(X1,Y1,'b-','linewidth',2);
-plot(P(rr,:,1),P(rr,:,2),'g*');
-plot(C(1,1),C(1,2),'r*');
-figure;
-imshow(a2);
-hold on
-plot(X2,Y2,'b-','linewidth',2);
-plot(C2(1,1),C2(1,2),'r*');
-plot(ps(rr,:,1),ps(rr,:,2),'g*');
-excelfile= 'test.xlsx';
-xlswrite(excelfile,ValuesInInches,'Sheet1',clm);
-
+Values = rounds(ValuesInInches);
+ excelfile= 'test2.xlsx';
+ %excelfile= 'check.xlsx';
+ xlswrite(excelfile,Values,'Hemanth',posColumn);
 
 end
 
